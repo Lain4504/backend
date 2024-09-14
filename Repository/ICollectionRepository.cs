02cb548663433dd820b950e69b79817cebaf02ab
Lain4504 <tienhuynhn2004@gmail.com>
2024-09-14 13:33:25 +0700

    Create Collection modules

---
+++ Repository/ICollectionRepository.cs
@@ -0,0 +1,13 @@
+﻿using BackEnd.Model;
+
+namespace BackEnd.Repository
+{
+    public interface ICollectionRepository
+    {
+        Task<Collection> GetCollectionByIdAsync(int id);
+        Task<List<Collection>> GetAllCollectionsAsync();
+        Task<Collection> SaveCollectionAsync(Collection collection);
+        Task<Collection> UpdateCollectionAsync(Collection collection);
+        Task DeleteCollectionAsync(int id);
+    }
+}
